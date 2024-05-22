12817ab5b5ee638297311fd9483ce82e2077819f
Riwan William Bououli <riwan.b@live.fr>
2024-05-22 11:16:37 +0200

    starting responsive

--- app/(landing)/_components/background-image.tsx
+++ app/(landing)/_components/background-image.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
-import bgImage from '@/public/background.webp';
+import bgImage from "@/public/background.webp";
 
 export const BackgroundImage = () => {
-    return <Image
+  return (
+    <div className="absolute w-full h-screen">
+      <Image
         src={bgImage}
         alt="Background"
         placeholder="blur"
         fill
         sizes="100vw"
         className="object-cover z-[-1]"
-    />
+      />
+    </div>
+  );
 };
--- app/(landing)/_components/navbar.tsx
+++ app/(landing)/_components/navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-
 import {
     NavigationMenu,
     NavigationMenuItem,
@@ -12,48 +11,36 @@ import { Button } from "@/components/ui/button";
 import { Logo } from "@/components/ui/logo";
 
 const navigation = [
-    {
-        item: "Home",
-    },
-    {
-        item: "About",
-    },
-    {
-        item: "Customers",
-    },
-    {
-        item: "Contact",
-    },
-    {
-        item: "Pricing",
-    },
-    {
-      item: "Contacts",
-  },
-]
+    { item: "Home" },
+    { item: "About" },
+    { item: "Customers" },
+    { item: "Contact" },
+    { item: "Pricing" },
+    { item: "Contacts" },
+];
 
 export const Navbar = () => {
     return (
         <div className="flex items-center justify-center w-full pt-8 select-none">
-            <NavigationMenu className={`w-full max-w-[1200px] justify-between px-5 py-2 h-[60px] items-center`}>
-                <NavigationMenuList>
+            <NavigationMenu className="w-full max-w-[1200px] flex-col md:flex-row justify-between px-5 py-2 h-auto md:h-[60px] items-center">
+                <NavigationMenuList className="flex justify-between w-full md:w-auto">
                     <NavigationMenuItem>
                         <Link href="/" className="flex items-center space-x-2">
                             <Logo />
-                            <h2 className="text-2xl font-bold">Networthing</h2>
+                            <h2 className="text-xl md:text-2xl font-bold">Networthing</h2>
                         </Link>
                     </NavigationMenuItem>
                 </NavigationMenuList>
-                <NavigationMenuList className="flex justify-between items-center font-light space-x-14 text-xs">
+                <NavigationMenuList className="grid grid-cols-3 gap-4 w-full md:flex md:justify-between md:items-center md:font-light md:space-y-0 md:space-x-14 md:text-xs mt-4 md:mt-0">
                     {navigation.map((link, index) => (
-                        <NavigationMenuItem key={index} className="cursor-pointer hover:underline">
+                        <NavigationMenuItem key={index} className="cursor-pointer hover:underline flex justify-center">
                             <NavigationMenuLink>
                                 {link.item}
                             </NavigationMenuLink>
                         </NavigationMenuItem>
                     ))}
                 </NavigationMenuList>
-                <NavigationMenuList>
+                <NavigationMenuList className="mt-4 md:mt-0">
                     <NavigationMenuItem>
                         <Link href="/">
                             <Button variant={"primary"} size={"lg"}>
@@ -64,5 +51,5 @@ export const Navbar = () => {
                 </NavigationMenuList>
             </NavigationMenu>
         </div>
-    )
+    );
 };
